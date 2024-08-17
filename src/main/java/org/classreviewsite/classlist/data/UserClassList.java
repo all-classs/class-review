b367d3c4937fe0b37f9423bf49c6406940342708
Kangmin Kim <76634341+amazon7737@users.noreply.github.com>
2024-08-17 20:02:34 +0900

    feat: 해당 유저가 개시된 강의를 이수하였는지에 대한 엔티티 작성

---
+++ src/main/java/org/classreviewsite/classlist/data/UserClassList.java
@@ -0,0 +1,50 @@
+package org.classreviewsite.classlist.data;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.classreviewsite.user.data.User;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Getter
+@Entity
+@Table(name = "UserClassList", uniqueConstraints = @UniqueConstraint(columnNames = {"classNumber", "userNumber"}))
+public class UserClassList {
+
+    // 수강한 고유 번호
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long CompletionNumber;
+
+    // 이수 구분
+    @Column(nullable = true, length = 45, unique = false)
+    private String CompletionType;
+
+    // 이수연도
+    @Column(nullable = true, length = 45, unique = false)
+    private String CompletionYear;
+
+    // 이수학년
+    @Column(nullable = true, length = 45, unique = false)
+    private String grade;
+
+    // 이수학기
+    @Column(nullable = true, length = 45, unique = false)
+    private String semester;
+
+    // 어떤 학생
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "userNumber")
+    private User userNumber;
+
+    // 어떤 교과목
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "classNumber")
+    private ClassList classNumber;
+
+
+}
