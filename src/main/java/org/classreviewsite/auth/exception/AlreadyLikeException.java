2213cc51624bf1e80cac3feecaaa41ec9d7ad67d
Kangmin Kim <76634341+amazon7737@users.noreply.github.com>
2024-08-17 19:47:12 +0900

    feat: 이미 좋아요하였을때 발생하는 예외 작성

---
+++ src/main/java/org/classreviewsite/auth/exception/AlreadyLikeException.java
@@ -0,0 +1,9 @@
+package org.classreviewsite.auth.exception;
+
+public class AlreadyLikeException extends RuntimeException{
+
+    public AlreadyLikeException(String message){
+        super(message);
+    }
+
+}
