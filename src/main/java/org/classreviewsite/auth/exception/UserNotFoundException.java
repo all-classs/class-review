b97615ae0c84d8a6bdf603c8949e89ca5861eb1a
Kangmin Kim <76634341+amazon7737@users.noreply.github.com>
2024-08-17 20:04:31 +0900

    feat: 해당 유저를 찾지 못했을때 발생하는 예외 작성

---
+++ src/main/java/org/classreviewsite/auth/exception/UserNotFoundException.java
@@ -0,0 +1,10 @@
+package org.classreviewsite.auth.exception;
+
+public class UserNotFoundException extends RuntimeException{
+
+    public UserNotFoundException(String message){
+        super(message);
+    }
+
+
+}
