a0b7069a38a43ba4fb04b508a8d4b6a7b5aba545
Kangmin Kim <76634341+amazon7737@users.noreply.github.com>
2024-08-17 19:52:32 +0900

    feat: 회원가입 요청 dto 작성

---
+++ src/main/java/org/classreviewsite/user/dto/CreateUserRequest.java
@@ -0,0 +1,25 @@
+package org.classreviewsite.user.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class CreateUserRequest {
+
+    private int userNumber;
+
+    private String password;
+
+    private String userName;
+
+    private String department;
+
+    private String nickname;
+
+
+}
