4c272d9d817253af511baa576e304d2b99e877ee
Kangmin Kim <76634341+amazon7737@users.noreply.github.com>
2024-08-17 19:50:49 +0900

    feat: 해당 강의를 찾지 못했을때 발생하는 예외 작성

---
+++ src/main/java/org/classreviewsite/auth/exception/ClassNotFoundException.java
@@ -0,0 +1,7 @@
+package org.classreviewsite.auth.exception;
+
+public class ClassNotFoundException extends RuntimeException{
+    public ClassNotFoundException(String message){
+        super(message);
+    }
+}
