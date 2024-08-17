8bbcf4abfc6851d3f8a10118874a28992ed0fc41
Kangmin Kim <76634341+amazon7737@users.noreply.github.com>
2024-08-17 19:51:36 +0900

    feat: ClassReview repository jpa 메소드 작성

---
+++ src/main/java/org/classreviewsite/review/infrastructure/ClassReviewDataRepository.java
@@ -0,0 +1,35 @@
+package org.classreviewsite.review.infrastructure;
+
+import org.classreviewsite.lecture.data.Lecture;
+import org.classreviewsite.review.data.ClassReview;
+import org.classreviewsite.user.data.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface ClassReviewDataRepository extends JpaRepository<ClassReview, Long> {
+
+
+    Optional<ClassReview> findByUserNumberAndLecId(@Param("userNumber") User userNumber, @Param("lecId") Lecture lecId);
+
+
+    @Query("select m from ClassReview m where m.lecId = :lecId order by m.likes desc limit 3")
+    List<ClassReview> findByLecIdOrderByLikesDesc(Long lecId );
+
+
+    @Query("select m from ClassReview m where m.userNumber.userNumber = :userNumber")
+    List<ClassReview> findByUserNumber(@Param("userNumber") int userNumber);
+
+
+    List<ClassReview> findByLecId(@Param("LectureId") Lecture lecId);
+
+
+
+
+
+}
