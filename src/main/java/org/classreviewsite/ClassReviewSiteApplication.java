20228b11020230c71dcd75546981b1e244cf9195
Kangmin Kim <76634341+amazon7737@users.noreply.github.com>
2024-08-17 18:51:53 +0900

    init: 프로젝트 설정

---
+++ src/main/java/org/classreviewsite/ClassReviewSiteApplication.java
@@ -0,0 +1,13 @@
+package org.classreviewsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClassReviewSiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClassReviewSiteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/classreviewsite/ClassReviewSiteApplicationTests.java
@@ -0,0 +1,13 @@
+package org.classreviewsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClassReviewSiteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
