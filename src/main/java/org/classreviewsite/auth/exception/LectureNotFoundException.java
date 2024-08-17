4f53562dc2aa9877ba9b89854a3acad36115c954
Kangmin Kim <76634341+amazon7737@users.noreply.github.com>
2024-08-17 19:55:51 +0900

    feat: 해당 강의를 찾지 못했을때 발생하는 예외 작성

---
+++ src/main/java/org/classreviewsite/auth/exception/LectureNotFoundException.java
@@ -0,0 +1,7 @@
+package org.classreviewsite.auth.exception;
+
+public class LectureNotFoundException extends RuntimeException{
+    public LectureNotFoundException(String message){
+        super(message);
+    }
+}
