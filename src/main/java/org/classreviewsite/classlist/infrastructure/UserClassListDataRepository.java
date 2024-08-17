9b106c9e646d824609d625e683e036d197747759
Kangmin Kim <76634341+amazon7737@users.noreply.github.com>
2024-08-17 20:03:13 +0900

    feat: UserClassList repository 작성 (학번별 조회, 학번별 조회 및 join classList)

---
+++ src/main/java/org/classreviewsite/classlist/infrastructure/UserClassListDataRepository.java
@@ -0,0 +1,22 @@
+package org.classreviewsite.classlist.infrastructure;
+
+import org.classreviewsite.user.data.User;
+import org.classreviewsite.classlist.data.UserClassList;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface UserClassListDataRepository extends JpaRepository<UserClassList, Long> {
+
+    Optional<UserClassList> findByUserNumber(@Param("userNumber") User userNumber);
+
+    @Query("select m from UserClassList m join fetch m.userNumber and join fetch m.classNumber where m.userNumber.userNumber = :userNumber")
+    List<UserClassList> findByUserNumberWithClassAndUser(@Param("userNumber") int userNumber);
+
+
+}
