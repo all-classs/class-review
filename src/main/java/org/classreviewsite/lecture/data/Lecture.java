03c252d8aba3d1b569e89f5a638e7b58568ea108
Kangmin Kim <76634341+amazon7737@users.noreply.github.com>
2024-08-27 14:25:49 +0900

    feat: 키워드 통계 시각화를 위한 중요도, 난이도, 재미 속성값 추가 작성, 정규화된 수치 속성값 추가, 해당 속성값들 update 변경감지 메소드 작성

--- src/main/java/org/classreviewsite/lecture/data/Lecture.java
+++ src/main/java/org/classreviewsite/lecture/data/Lecture.java
@@ -5,6 +5,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.ColumnDefault;
 
 @NoArgsConstructor
 @AllArgsConstructor
@@ -40,25 +41,83 @@ public class Lecture {
     @Enumerated(EnumType.STRING)
     private LectureType lectureType;
 
+    @Column(nullable = false)
+    private Double important;
 
-    public void updateAverageStarLating(Long updateLating){
+    @Column(nullable = false)
+    private Double difficulty;
+
+    @Column(nullable = false)
+    private Double funny;
+
+
+    @ColumnDefault("0")
+    @Column(nullable = true)
+    private Double importantNormalization;
+
+    @Column(nullable = true)
+    @ColumnDefault("0")
+    private Double difficultyNormalization;
+
+    @Column(nullable = true)
+    @ColumnDefault("0")
+    private Double funnyNormalization;
+
+
+    public Long updateAverageStarLating(Long updateLating){
         this.averageStarLating = updateLating;
+        return this.averageStarLating;
     }
 
-    public void updateTotalStarLating(Long plusLating){
+    public Long updateTotalStarLating(Long plusLating){
         this.totalStarLating = totalStarLating + plusLating;
+        return this.totalStarLating;
     }
 
-    public void updateReviewCount(){
+    public Long addReviewCount(Lecture lecture){
+
         this.reviewCount = reviewCount+1;
+        return this.reviewCount;
     }
 
-    public void cancelTotalStarLating(Long minusLating){
+    public Long cancelTotalStarLating(Long minusLating){
         this.totalStarLating = totalStarLating - minusLating;
+        return this.totalStarLating;
     }
 
-    public void cancelReviewCount(){
+    public Long cancelReviewCount(){
         this.reviewCount = reviewCount - 1;
+        return this.reviewCount;
+    }
+
+    public Double updateImportant(Double important){
+        this.important = important;
+        return this.important;
+    }
+
+    public Double updateDifficulty(Double difficulty){
+        this.difficulty = difficulty;
+        return this.difficulty;
+    }
+
+    public Double updateFunny(Double funny){
+        this.funny = funny;
+        return this.funny;
+    }
+
+    public Double updateImportantNormalization(Double important){
+        this.importantNormalization = important;
+        return this.importantNormalization;
+    }
+
+    public Double updateDifficultyNormalization(Double difficulty){
+        this.difficultyNormalization = difficulty;
+        return this.difficultyNormalization;
+    }
+
+    public Double updateFunnyNormalization(Double funny){
+        this.funnyNormalization  = funny;
+        return this.funnyNormalization;
     }
 
 
