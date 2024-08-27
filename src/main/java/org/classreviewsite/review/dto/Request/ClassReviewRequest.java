13f6df1d0a7508e5c6dfa7f3aea13661bfb211ea
Kangmin Kim <76634341+amazon7737@users.noreply.github.com>
2024-08-27 14:22:23 +0900

    fix: 수강후기 요청에 관한 메소드 패턴 작성

--- src/main/java/org/classreviewsite/review/dto/Request/ClassReviewRequest.java
+++ src/main/java/org/classreviewsite/review/dto/Request/ClassReviewRequest.java
@@ -3,14 +3,45 @@ package org.classreviewsite.review.dto.Request;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import org.classreviewsite.review.data.ClassReview;
 
 @Builder
 @Getter
 @AllArgsConstructor
 public class ClassReviewRequest {
-    private String LecName;
-    private Long UserNumber;
-    private Long StarLating;
-    private String PostTitle;
-    private String PostContent;
+    private String lectureName;
+    private Long userNumber;
+    private Long starLating;
+    private String postTitle;
+    private String postContent;
+
+    private Long important;
+    private Long funny;
+    private Long difficulty;
+
+    public static ClassReviewRequest from(ClassReview review){
+        return new ClassReviewRequest(
+                review.getLecId().getLectureName(),
+                Long.valueOf(review.getUserNumber().getUserNumber()),
+                review.getStarLating(),
+                review.getPostTitle(),
+                review.getPostContent(),
+                review.getImportant(),
+                review.getFunny(),
+                review.getDifficulty()
+        );
+    }
+
+    public static ClassReviewRequest of(String postTitle, String postContent, Long userNumber, Long starLating, String lectureName, Long important, Long funny, Long difficulty){
+        return new ClassReviewRequest(
+                lectureName,
+                userNumber,
+                starLating,
+                postTitle,
+                postContent,
+                important,
+                funny,
+                difficulty
+        );
+    }
 }
