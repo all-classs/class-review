d00ed7434b0227520a20140411be366d32073f10
Kangmin Kim <76634341+amazon7737@users.noreply.github.com>
2024-08-27 15:02:09 +0900

    delete: 사용하지 않는 dto , Controller 삭제

--- src/main/java/org/classreviewsite/lecture/infrastructure/ImageUrlDataRepository.java
+++
@@ -1,9 +0,0 @@
-package org.classreviewsite.lecture.infrastructure;
-
-import org.classreviewsite.classlist.data.ImageUrl;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ImageUrlDataRepository extends JpaRepository<ImageUrl, Long> {
-}
