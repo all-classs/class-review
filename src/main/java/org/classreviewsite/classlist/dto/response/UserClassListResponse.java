6c56e6f8216592d176a2afaf3439d5ec6481b67e
Kangmin Kim <76634341+amazon7737@users.noreply.github.com>
2024-08-27 14:39:08 +0900

    feat: 들은 강의 목록을 반환하기 위해 toList, from 메소드 작성

--- src/main/java/org/classreviewsite/classlist/dto/response/UserClassListResponse.java
+++ src/main/java/org/classreviewsite/classlist/dto/response/UserClassListResponse.java
@@ -3,6 +3,10 @@ package org.classreviewsite.classlist.dto.response;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import org.classreviewsite.classlist.data.UserClassList;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Data
 @AllArgsConstructor
@@ -24,6 +28,31 @@ public class UserClassListResponse {
     private String lectureName;
 
     private String professorName;
+
+    public static List<UserClassListResponse> toList(List<UserClassList> list){
+        List<UserClassListResponse> result = new ArrayList<>();
+        for(UserClassList userClassList : list){
+            result.add(
+                UserClassListResponse.from(userClassList)
+            );
+        }
+        return result;
+    }
+
+    public static UserClassListResponse from(UserClassList userClassList){
+        return UserClassListResponse.builder()
+                .classNumber(userClassList.getClassNumber().getClassNumber())
+                .semester(userClassList.getSemester())
+                .grade(userClassList.getGrade())
+                .CompletionType(userClassList.getCompletionType())
+                .CompletionYear(userClassList.getCompletionYear())
+                .CompletionNumber(userClassList.getCompletionNumber())
+                .lectureName(userClassList.getClassNumber().getLecture().getLectureName())
+                .professorName(userClassList.getClassNumber().getProfessor().getProfessorName())
+                .build();
+    }
+
+
 
 
 }
