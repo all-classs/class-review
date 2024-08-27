3fcd08e630ca7b31ba75409fd0771f828fded20a
Kangmin Kim <76634341+amazon7737@users.noreply.github.com>
2024-08-27 14:09:03 +0900

    feat: 개설된 강의 repository 작성 (강의 번호를 통해서 조회, 학교를 통해서 조회, 교수 이름을 통해서 조회하는 메소드 작성)

--- src/main/java/org/classreviewsite/classlist/infrastructure/ClassListDataRepository.java
+++ src/main/java/org/classreviewsite/classlist/infrastructure/ClassListDataRepository.java
@@ -13,12 +13,17 @@ import java.util.Optional;
 public interface ClassListDataRepository extends JpaRepository<ClassList, Long> {
 
 
-    @Query("select m from ClassList m join fetch m.lecture")
-    List<ClassList> findByClassListWithProfessorName();
-
-
-
     @Query("select m from ClassList m join fetch m.lecture where m.lecture.lectureId = :lectureId")
     Optional<ClassList> findByLectureIdWithProfessorName(@Param("lectureId") Long lectureId);
+
+    @Query("select m from ClassList m join fetch m.lecture join fetch m.professor join fetch m.captainImage where m.lecture.university = :university")
+    List<ClassList> findClassListByUniversity(@Param("university") String university);
+
+    @Query("select m from ClassList m join fetch m.lecture join fetch m.professor join fetch m.captainImage where m.lecture.university = :university and m.lecture.lectureId = :lectureId")
+    List<ClassList> findClassListByUniversityAndLectureId(@Param("university") String university, @Param("lectureId") Long lectureId);
+
+    @Query("select m from ClassList m join fetch m.lecture join fetch m.captainImage where m.professor.professorName = :professorName")
+    List<ClassList> findClassListByProfessorName(String professorName);
+
 
 }
