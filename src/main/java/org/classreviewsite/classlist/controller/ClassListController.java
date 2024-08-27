668d0c1b31225f04e637b526222cc4564de09a41
Kangmin Kim <76634341+amazon7737@users.noreply.github.com>
2024-08-27 14:08:07 +0900

    feat: 개설된 강의 RestController 작성

--- src/main/java/org/classreviewsite/classlist/controller/ClassListController.java
+++ src/main/java/org/classreviewsite/classlist/controller/ClassListController.java
@@ -1,12 +1,18 @@
 package org.classreviewsite.classlist.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
+import org.classreviewsite.classlist.dto.response.ClassListInfo;
 import org.classreviewsite.classlist.dto.response.ClassListWithProfessorName;
 import org.classreviewsite.classlist.dto.response.UserClassListResponse;
 import org.classreviewsite.classlist.service.ClassListService;
 import org.classreviewsite.classlist.service.UserClassListService;
+import org.classreviewsite.lecture.dto.DepartmentResponse;
+import org.classreviewsite.lecture.service.LectureService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,32 +21,51 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
+@Tag(name = "강의 정보 API", description = "강의 정보 관련 요청입니다.")
 public class ClassListController {
 
     private final UserClassListService userClassListService;
 
     private final ClassListService classListService;
 
+    private final LectureService lectureService;
 
     @GetMapping("/class")
+    @Operation(summary = "전체 강의 정보", description = "university(학교이름)을 param으로 요청하시면 해당 학교의 강의정보가 조회됩니다. 해당 학교의 강의정보가 없을경우 401 상태번호를 반환합니다.")
+    @ApiResponse(responseCode = "200", description = "전체 강의 목록입니다.")
+    @ApiResponse(responseCode = "401", description = "해당 학교의 강의가 존재하지 않습니다.")
+    public Result findClassList(@RequestParam(value = "university") String university, @RequestParam(value = "lectureId", required = false) Long lectureId){
+
+        if(lectureId==null){
+            List<ClassListInfo> response = classListService.findClassListByUniversity(university);
+            return new Result(200, response, "전체 강의 목록입니다.");
+        }
+
+        ClassListWithProfessorName.ClassListWithProfessorNameInDetail response = classListService.findByClassListWithProfessorNameInDetail(lectureId);
+        return new Result(200, response, "강의 상세 정보 조회입니다.");
+    }
+
+    @GetMapping("/class/me")
+    @Operation(summary = "나의 수강 정보", description = "userNumber(학번)을 param으로 요청하시면 해당 학생의 수강 정보가 조회됩니다.")
+    @ApiResponse(responseCode = "200", description = "해당 학생의 수강한 강의 목록입니다.")
+    @ApiResponse(responseCode = "401", description = "해당 학생이 수강한 강의는 없습니다.")
     public Result findUserClassList(@RequestParam("userNumber") int userNumber){
 
         List<UserClassListResponse> response = userClassListService.findClassForSemester(userNumber);
 
         return new Result(200, response, "해당 학생의 수강한 강의 목록입니다.");
+
     }
 
-    @GetMapping("/lecture")
-    public Result findLectureListAll(){
-        List<ClassListWithProfessorName> list = classListService.findByClassListWithProfessorName();
-        return new Result(200, list, "강의 전체 목록 조회입니다.");
+    @GetMapping("/department")
+    @Operation(summary = "학과 전체조회" , description = " 기존 강의 목록에서 학과들을 추려 중복을 제거하고 출력하고있습니다.")
+    @ApiResponse(responseCode = "200", description = "학과 전체조회입니다.")
+    public Result findDepartmentRequest(){
+        List<String> departmentList =  lectureService.findDistinctDepartment();
+        DepartmentResponse response = new DepartmentResponse(200, departmentList, "학과 전체조회입니다.");
+        return new Result<>(200, response, "학과 전체조회입니다.");
     }
 
-    @GetMapping("/lecture/detail")
-    public Result findLectureDetail(@RequestParam("lectureId") Long lectureId){
-        ClassListWithProfessorName.ClassListWithProfessorNameInDetail response = classListService.findByClassListWithProfessorNameInDetail(lectureId);
-        return new Result(200, response, "강의 상세 정보 조회입니다.");
-    }
 
 
     @Data
