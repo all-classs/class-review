4a434f655261e3f00c21a622536a333450a0a576
Kangmin Kim <76634341+amazon7737@users.noreply.github.com>
2024-08-27 14:30:12 +0900

    fix: 좋아요 기능 전달값에 문제가 발생하여 데이터베이스 속성값이 수정되지 않는 버그 수정, 인기도 중요도 재미가 갱신되도록 추가작성

--- src/main/java/org/classreviewsite/review/service/ReviewService.java
+++ src/main/java/org/classreviewsite/review/service/ReviewService.java
@@ -8,6 +8,8 @@ import org.classreviewsite.auth.exception.UserNotFoundException;
 import org.classreviewsite.lecture.data.Lecture;
 import org.classreviewsite.review.data.Likes;
 import org.classreviewsite.review.data.ClassReview;
+import org.classreviewsite.review.dto.Request.DeleteReviewRequest;
+import org.classreviewsite.review.dto.Response.ReviewInfo;
 import org.classreviewsite.user.data.User;
 import org.classreviewsite.lecture.service.LectureService;
 import org.classreviewsite.review.dto.Request.LikeRequest;
@@ -34,13 +36,13 @@ public class ReviewService {
     private final UserService userService;
 
     @Transactional(readOnly = true)
-    public List<ClassReview> findAll(){
-        List<ClassReview> list = classReviewDataRepository.findAll();
+    public List<ReviewInfo> findAll(Long lectureId){
+        List<ClassReview> list = classReviewDataRepository.findAll(lectureId);
         if(list.isEmpty()){
             throw new ReviewNotFoundException("수강 후기가 어디에도 없습니다.");
         }
 
-        return list;
+        return ReviewInfo.toList(list);
     }
 
 
@@ -48,7 +50,7 @@ public class ReviewService {
     @Transactional
     public Long addReviewPost(ClassReviewRequest request){
 
-        Lecture foundLecture = lectureService.findBylectureName(request.getLecName());
+        Lecture foundLecture = lectureService.findBylectureName(request.getLectureName());
 
         User foundUser = userService.findById(request.getUserNumber()).orElseThrow(() -> new UserNotFoundException("존재하지 않는 학생입니다."));
 
@@ -59,17 +61,34 @@ public class ReviewService {
                 .likes(0)
                 .postTitle(request.getPostTitle())
                 .postContent(request.getPostContent())
+                .important(request.getImportant())
+                .difficulty(request.getDifficulty())
+                .funny(request.getFunny())
                 .build();
 
         validateAddReviewPost(foundUser, foundLecture);
 
-        Long resultStarLating = classReviewDataRepository.save(classReview).getStarLating();
+        Long id = classReviewDataRepository.save(classReview).getReviewId();
 
-        lectureService.updateReviewCount(foundLecture);
-        lectureService.updateTotalStarLating(foundLecture, request.getStarLating());
-        lectureService.updateAverageStarLating(foundLecture);
+        Double funny = lectureService.updateFunnyAsPlus(foundLecture, classReview);
+        Double difficulty = lectureService.updateDifficultyAsPlus(foundLecture, classReview);
+        Double important = lectureService.updateImportantAsPlus(foundLecture, classReview);
+        Double total = funny+difficulty+important;
 
-        return resultStarLating;
+
+
+        Long count = lectureService.addReviewCount(foundLecture);
+        Long star = lectureService.updateTotalStarLating(foundLecture, request.getStarLating());
+        Long result =  lectureService.updateAverageStarLating(foundLecture, star, count);
+
+        if(foundLecture.getReviewCount()>0){
+            lectureService.updateFunnyNormalization(foundLecture, funny/total);
+            lectureService.updateDifficultyNormalization(foundLecture, difficulty/total);
+            lectureService.updateImportNormalization(foundLecture, important/total);
+        }
+
+        return id;
+
 
     }
 
@@ -84,42 +103,63 @@ public class ReviewService {
     }
 
     @Transactional
-    public void updateReviewPost(UpdateReviewRequest request){
-        ClassReview post = new ClassReview();
+    public Long updateReviewPost(UpdateReviewRequest request){
+        ClassReview post = classReviewDataRepository.findById(request.getPostId()).orElseThrow(() -> new ReviewNotFoundException("해당 수강후기가 존재하지 않습니다."));
         post.updatePost(request.getPostTitle(), request.getPostContent());
-
+        return post.getReviewId();
     }
 
     @Transactional
-    public void deleteReviewPost(Long postId){
-        ClassReview deletedClassReview = classReviewDataRepository.findById(postId).orElseThrow(() -> new NullPointerException("해당 강의가 존재하지 않습니다."));
+    public void deleteReviewPost(DeleteReviewRequest request){
+        User user = userService.findById(Long.valueOf(request.getUserNumber())).orElseThrow(() -> new UserNotFoundException("해당 학생이 존재하지 않습니다."));
 
-        classReviewDataRepository.deleteById(postId);
+        ClassReview deletedClassReview = classReviewDataRepository.findByReviewIdAndUserNumber(request.getPostId(), user).orElseThrow(() -> new NullPointerException("해당 수강후기가 존재하지 않습니다."));
+
+        classReviewDataRepository.deleteById(request.getPostId());
 
         likesDataRepository.deleteAllByClassReview(deletedClassReview);
 
         Lecture updateLecture = lectureService.findById(deletedClassReview.getLecId().getLectureId());
 
+        Double difficulty = lectureService.updateDifficultyAsMinus(updateLecture, deletedClassReview);
+        Double important = lectureService.updateImportantAsMinus(updateLecture, deletedClassReview);
+        Double funny = lectureService.updateFunnyAsMinus(updateLecture, deletedClassReview);
+        Double total = difficulty+important+funny;
 
-        lectureService.cancelReviewCount(updateLecture);
-        lectureService.cancelTotalStarLating(updateLecture, deletedClassReview.getStarLating());
-        lectureService.updateAverageStarLating(updateLecture);
+
+        Long count = lectureService.cancelReviewCount(updateLecture);
+        Long star = lectureService.cancelTotalStarLating(updateLecture, deletedClassReview.getStarLating());
+        lectureService.updateAverageStarLating(updateLecture, star, count);
+
+        if(updateLecture.getReviewCount()>0){
+            lectureService.updateImportNormalization(updateLecture, important/total);
+            lectureService.updateFunnyNormalization(updateLecture, funny/total);
+            lectureService.updateDifficultyNormalization(updateLecture, difficulty/total);
+        }
+
+
 
     }
 
     @Transactional
-    public void addLikeReviewPost(LikeRequest request){
+    public String addLikeReviewPost(LikeRequest request){
+
         // userNumber , reviewId
-        User user = userService.findById(request.getUserNumber()).orElseThrow(() -> new UserNotFoundException("해당 학생이 존재하지 않습니다."));
+        User user = userService.findById(Long.valueOf(request.getUserNumber())).orElseThrow(() -> new UserNotFoundException("해당 학생이 존재하지 않습니다."));
 
-        ClassReview classReview = classReviewDataRepository.findById(request.getReviewId()).orElseThrow(() -> new NullPointerException("해당 수강후기가 존재하지 않습니다."));
+        ClassReview classReview = classReviewDataRepository.findById(request.getPostId()).orElseThrow(() -> new NullPointerException("해당 수강후기가 존재하지 않습니다."));
 
-        // 좋아요 중복 검사
-        validateLikeReviewPost(user, classReview);
+        try{
+            validateLikeReviewPost(user, classReview);
+        }catch (AlreadyLikeException exception){
+            int like = classReview.cancelLike(classReview.getLikes()-1);
+            likesDataRepository.deleteByClassReviewAndUser(classReview, user);
+            return "좋아요가 취소되었습니다.";
+        }
 
-        ClassReview likedPost = new ClassReview();
 
-        likedPost.like();
+        int like = classReview.like();
+
         Likes likesUser = Likes.builder()
                 .classReview(classReview)
                 .user(user)
@@ -127,21 +167,21 @@ public class ReviewService {
 
         likesDataRepository.save(likesUser);
 
+        return "좋아요가 추가되었습니다.";
+
     }
 
-    @Transactional(readOnly = true)
+//    @Transactional
     public void validateLikeReviewPost(User user , ClassReview classReview){
         likesDataRepository.findByUserAndClassReview(user, classReview)
                 .ifPresent(m -> {
-                    cancelLikeReviewPost(user, classReview);
+            throw new AlreadyLikeException("좋아요가 취소 되었습니다.");
                 }
                 );
     }
 
 
-    @Transactional
     public void cancelLikeReviewPost(User user , ClassReview classReview){
-        classReview.cancelLike();
         likesDataRepository.deleteByClassReviewAndUser(classReview, user);
 
         throw new AlreadyLikeException("좋아요가 취소되었습니다.");
@@ -177,11 +217,5 @@ public class ReviewService {
         }
         return list;
     }
-
-
-//    @Transactional(readOnly = true)
-//    public List<ReviewPost> findByPostDetail(){
-//
-//    }
 
 }
