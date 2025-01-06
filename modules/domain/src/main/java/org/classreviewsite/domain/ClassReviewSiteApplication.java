cafa27a9eb90dcd60e7b4b82ae48c3372fd89daf
Kangmin Kim <76634341+amazon7737@users.noreply.github.com>
2025-01-06 13:24:04 +0900

    init: 멀티모듈 구조로 생성

---
+++ modules/domain/src/main/java/org/classreviewsite/domain/ClassReviewSiteApplication.java
@@ -0,0 +1,13 @@
+package org.classreviewsite.domain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClassReviewSiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClassReviewSiteApplication.class, args);
+    }
+
+}
