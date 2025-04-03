574a8f0a409c86a048fdb9f6b35a0ab46c5a901d
Kangmin Kim <76634341+amazon7737@users.noreply.github.com>
2025-04-03 23:04:36 +0900

    fix: 회원가입 메소드 인증정보도 추가되도록 변경, findById 예외처리

--- src/main/java/org/classreviewsite/user/service/UserService.java
+++ src/main/java/org/classreviewsite/user/service/UserService.java
@@ -2,6 +2,9 @@ package org.classreviewsite.user.service;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.classreviewsite.auth.data.Authority;
+import org.classreviewsite.auth.exception.UserNotFoundException;
+import org.classreviewsite.auth.service.AuthService;
 import org.classreviewsite.user.dto.CreateUserRequest;
 import org.classreviewsite.user.data.User;
 import org.classreviewsite.auth.exception.UserExistException;
@@ -20,37 +23,31 @@ public class UserService {
 
     private final UserDataRepository userDataRepository;
     private final PasswordEncoder passwordEncoder;
+    private final AuthService authService;
 
     @Transactional(readOnly = true)
-    public Optional<User> findById(Long userNumber){
-        return userDataRepository.findById(userNumber);
+    public User findById(Long userNumber){
+        return userDataRepository.findById(userNumber).orElseThrow(() -> new UserNotFoundException("해당 학생이 존재하지 않습니다."));
     }
-
 
     @Transactional
-    public void signUp(CreateUserRequest user){
+    public int signUp(CreateUserRequest user){
 
         validateDuplicateUser(user);
-
         String password = passwordEncoder.encode(user.getPassword());
 
-        userDataRepository.save(User.toEntity(user, password));
+        Authority authority = Authority.toEntity(user.getUserType());
+        int userNumber =  userDataRepository.save(User.toEntity(user, password, authority)).getUserNumber();
+
+        return userNumber;
+
 
     }
 
-
     private void validateDuplicateUser(CreateUserRequest user){
-
        userDataRepository.findByUserNumber(user.getUserNumber())
                .ifPresent(m -> {
             throw new UserExistException("이미 존재하는 학생입니다.");
         });
-
-
     }
-
-
-
-
-
 }
