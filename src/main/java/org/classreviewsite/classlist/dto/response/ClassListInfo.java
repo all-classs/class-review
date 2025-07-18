c181cfae7e95a9a66e45dedfc8a7c1c353653a93
Kangmin Kim <76634341+amazon7737@users.noreply.github.com>
2025-07-19 02:08:38 +0900

    fix: 평점 클래스 변경으로 인한 ClassList 관련 코드 수정

--- src/main/java/org/classreviewsite/classlist/dto/response/ClassListInfo.java
+++ src/main/java/org/classreviewsite/classlist/dto/response/ClassListInfo.java
@@ -2,6 +2,7 @@ package org.classreviewsite.classlist.dto.response;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import org.classreviewsite.auth.util.NumberFormat;
 import org.classreviewsite.classlist.data.ClassList;
 import org.classreviewsite.lecture.data.Lecture;
 import org.classreviewsite.lecture.data.LectureType;
@@ -15,51 +16,23 @@ import java.util.List;
 public class ClassListInfo {
 
     private Long lectureId;
-
-//    private Long classNumber;
-
     private String lectureName;
-
     private String department;
-
     private String university;
-
     private LectureType lectureType;
-
-    private Long averageStarLating;
-
+    private Double averageStarLating;
     private String professor;
-
     private boolean opened;
-
     private String icon;
 
-    public static class ClassListInfoWithMyPage{
-
-    }
-
-
-    public static List<ClassListInfo> toList(List<ClassList> classLists){
-
-        List<ClassListInfo> list =  new ArrayList<>();
-
-        for(ClassList classList: classLists){
-            list.add(ClassListInfo.from(classList));
-        }
-        return list;
-
-    }
-
     public static ClassListInfo from(ClassList classList){
-
         return new ClassListInfo(
                 classList.getLecture().getLectureId(),
-//                classList.getClassNumber(),
                 classList.getLecture().getLectureName(),
                 classList.getLecture().getDepartment(),
                 classList.getLecture().getUniversity(),
                 classList.getLecture().getLectureType(),
-                classList.getLecture().getAverageStarLating(),
+                NumberFormat.format(classList.getLecture().getStarRating().getAverageRating()),
                 classList.getProfessor().getProfessorName(),
                 classList.isOpened(),
                 classList.getIcon().getImageUrl()
