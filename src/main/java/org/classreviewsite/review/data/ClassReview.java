777378046d857ce796de2a7e5568c5a92d5e1f9f
Kangmin Kim <76634341+amazon7737@users.noreply.github.com>
2025-07-19 02:10:50 +0900

    fix: create 메소드 추가, 좋아요 로직 변경, 속성값 삭제

--- src/main/java/org/classreviewsite/review/data/ClassReview.java
+++ src/main/java/org/classreviewsite/review/data/ClassReview.java
@@ -28,7 +28,7 @@ public class ClassReview extends BaseTimeEntity {
     private String postTitle;
 
     @Column(nullable = false, unique = false)
-    private Long starLating;
+    private Double starLating;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "userNumber" , nullable = false, unique = false)
@@ -59,23 +59,27 @@ public class ClassReview extends BaseTimeEntity {
         return this.likes;
     }
 
-    /**
-     * 좋아요 취소
-     *
-     */
-    public int cancelLike(int like){
+    public int like(int like){
         this.likes = like;
         return this.likes;
     }
 
-    /**
-     * 글 수정
-     */
-    public void updatePost(String postTitle, String postContent){
+    public void update(String postTitle, String postContent, Double starLating, Long important, Long difficulty, Long funny){
         this.postTitle = postTitle;
         this.postContent = postContent;
+        this.starLating = starLating;
     }
 
+    public static ClassReview create(Lecture lecture, User user, Double starLating, int likes, String postContent, String postTitle){
+        return ClassReview.builder()
+                .lecId(lecture)
+                .userNumber(user)
+                .starLating(starLating)
+                .likes(likes)
+                .postTitle(postTitle)
+                .postContent(postContent)
+                .build();
+    }
 
 
 }
