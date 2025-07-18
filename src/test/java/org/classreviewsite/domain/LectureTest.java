a0d00e33c6893aeab30ee9ca82a829a4e33536ac
Kangmin Kim <76634341+amazon7737@users.noreply.github.com>
2025-07-19 02:01:13 +0900

    test: 수강후기 작성시 별점 추가, 감소 테스트

---
+++ src/test/java/org/classreviewsite/domain/LectureTest.java
@@ -0,0 +1,47 @@
+package org.classreviewsite.domain;
+
+import org.assertj.core.api.Assertions;
+import org.classreviewsite.lecture.data.Lecture;
+import org.classreviewsite.lecture.data.LectureType;
+import org.classreviewsite.review.data.StarRating;
+import org.classreviewsite.review.dto.Request.ClassReviewRequest;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+@ExtendWith(MockitoExtension.class)
+public class LectureTest {
+
+    Lecture lecture;
+
+    @BeforeEach
+    void setUp() {
+        lecture = new Lecture(12345L, "강의명", StarRating.createRatingBuilder(), "학과명", "학교명", LectureType.교양선택);
+    }
+
+    @Test
+    @DisplayName("수강후기를 작성하면, 별점이 추가된다.")
+    void createReview() {
+        ClassReviewRequest classReviewRequest = ClassReviewRequest.of("", "", 20201234L, 4.0, "강의명");
+
+        lecture.addReview(classReviewRequest.getStarLating());
+
+        assertThat(lecture.getStarRating().getAverageRating()).isEqualTo(4.0);
+        assertThat(lecture.getStarRating().getTotalRating()).isEqualTo(4.0);
+    }
+
+    @Test
+    @DisplayName("수강후기를 삭제하면, 별점이 감소된다.")
+    void removeReview() {
+        lecture.addReview(4.0);
+        assertThat(lecture.getStarRating().getTotalRating()).isEqualTo(4.0);
+
+        lecture.removeReview(4.0);
+        assertThat(lecture.getStarRating().getTotalRating()).isEqualTo(0.0);
+    }
+}
